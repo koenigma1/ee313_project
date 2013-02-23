7ec7f221360d63ded1f16db19dd9bc81655a89ea
Mark Allen Koenig <makoenig@corn06.stanford.edu>
2013-02-23 15:29:46 -0800

    adding sizing folder to test commit

---
+++ sizing/path.py
@@ -0,0 +1,36 @@
+'''
+  optimize sizing over a path
+'''
+
+
+class Path():
+  def __init__(self, inputCap, loadCap):
+    self.stages = []
+    self.inputCap = inputCap
+    self.loadCap = loadCap
+
+  def pathEffort(self):
+    pe = float(self.loadCap) / self.inputCap
+    for stage in self.stages:
+      pe *= (stage.le*stage.be)
+    self.pe = pe
+    return self.pe
+
+  def optimalStageEffort(self):
+    self.se = self.pe ** (1.0/len(self.stages))
+    return self.se
+   
+  def size(self):
+    self.optimalStageEffort()
+    lastW = self.loadCap
+    for stage in reversed(self.stages):
+      lastW = stage.size(self.se, lastW)
+
+  def show(self):
+    print "Stage\tGate\tWp\tWn\tCin\tLE\tgamma"
+    for i,stage in enumerate(self.stages):
+      print "%i\t" % i,
+      stage.show()
+      
+
+
---
+++ sizing/task1.py
@@ -0,0 +1,28 @@
+'''
+  simulate a static 8-stage decoder
+'''
+
+from path import Path
+from stage import nand, inv
+
+def main():
+  # build the model
+  p = Path(24, 4505.6)
+  p.stages = [nand(3, be=2),
+              inv(),
+              nand(2, be=4),
+              inv(),
+              inv(),
+              inv(),
+              nand(2, be=16),
+              inv()]
+  print p.stages
+  print "path effort:", p.pathEffort()
+  print "stage effort:", p.optimalStageEffort()
+  print p.stages
+  p.size()
+  p.show()
+
+
+if __name__ == '__main__':
+  main()
